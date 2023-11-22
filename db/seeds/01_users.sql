9aaede0d2dffe6b83f671e3fdb8cb3e1dc80dfbf
Peter Hashem <peterhashem@gmail.com>
2023-11-22 18:08:57 -0500

    added users

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,4 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (name, email, password) VALUES ('Peter', 'peterhashem@gmail.com', '123');
+INSERT INTO users (name, email, password) VALUES ('Katie', 'katiekucsera@gmail.com', '456');
+INSERT INTO users (name, email, password) VALUES ('Tom', 'thomas.spencer2034@gmail.com', '789');
