0c7baa3a1e7a4448cd16918c484815522db71e8b
Peter Hashem <peterhashem@gmail.com>
2023-11-24 22:45:30 -0500

    added values into categories

---
+++ db/seeds/02_categories.sql
@@ -0,0 +1,5 @@
+INSERT INTO categories (name) VALUES ('food');
+INSERT INTO categories (name) VALUES ('movies');
+INSERT INTO categories (name) VALUES ('books');
+INSERT INTO categories (name) VALUES ('products');
+INSERT INTO categories (name) VALUES ('miscellaneous');
