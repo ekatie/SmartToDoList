9261cbf09b3f36d2419b6909be3f380914aa8135
Peter Hashem <peterhashem@gmail.com>
2023-11-24 23:07:25 -0500

    added seeds to tasks table

--- db/seeds/05_tasks.sql
+++ db/seeds/05_tasks.sql
@@ -1,12 +1,12 @@
--- Widgets table seeds here (Example)
-INSERT INTO tasks (user_id, description, category, status, created_date, completed_date, priority, due_date) VALUES
-(1, 'Eat at Omnom Yummy Eatz', 'food', FALSE, NOW(), NULL, TRUE, NULL),
-(1, 'Watch Lord of the Rings Trilogy', 'movie', FALSE, NOW(), NULL, TRUE, NULL),
-(1, 'Buy Games from Autumn Steam Sale', 'product', FALSE, NOW(), NULL, TRUE, NULL),
-(2, 'Read Longest Book Ever', 'book', FALSE, NOW(), NULL, FALSE, NULL),
-(2, 'Code all day', 'miscellaneous', FALSE, NOW(), NULL, TRUE, NULL),
-(2, 'Eat Falafel', 'food', FALSE, NOW(), NULL, TRUE, NULL),
-(3, 'Watch The Matrix', 'movie', FALSE, NOW(), NULL, FALSE, NULL),
-(3, 'Buy Superman Comic', 'product', FALSE, NOW(), NULL, TRUE, NULL);
-
+-- tasks table seeds here (Example)
+INSERT INTO tasks (user_id, category_id, description, status, created_date, completed_date, priority, due_date) VALUES
+(1, 1, 'Eat at Omnom Yummy Eatz', FALSE, NOW(), NULL, TRUE, NULL),
+(1, 2, 'Watch Lord of the Rings Trilogy', FALSE, NOW(), NULL, FALSE, NULL),
+(1, 4, 'Buy games from Autumn Steam Sale', FALSE, NOW(), NULL, TRUE, NULL),
+(2, 3, 'Read Longest Book Ever', FALSE, NOW(), NULL, TRUE, NULL),
+(2, 5, 'Code all day', TRUE, NOW(), NULL, TRUE, NULL),
+(2, 1, 'Eat Falafel', FALSE, NOW(), NULL, TRUE, NULL),
+(3, 2, 'Watch The Matrix', FALSE, NOW(), NULL, FALSE, NULL),
+(3, 4, 'Buy Superman Comic', FALSE, NOW(), NULL, FALSE, NULL),
+(3, 1, 'Eat Poutine', FALSE, NOW(), NULL, TRUE, NULL);
 
