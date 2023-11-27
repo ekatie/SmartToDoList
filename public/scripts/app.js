b2cfd844bb415dcdb013c7e7db7afdbf57b695a6
Tom Spencer <thomas.spencer2034@gmail.com>
2023-11-27 10:02:37 -0500

    Add html for list item to app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,8 +8,29 @@ $(document).ready(function() {
  const createListElement = function(item) {
     //Create hard coded list items
     const $tweet = $(`<article>
-          <div class = 'item-container'>
-          <div>
-        </article>`);
+      <div class="list-container">
+        <div class="left-column">
+          <% if("isChecked"==="isChecked" ){ %>
+            <i class="fa-regular fa-square-check fa-2xl"></i>
+            <% } else{ %>
+              <i class="fa-regular fa-square fa-2xl"></i>
+              <% } %>
+                <i class="fa-solid ${item.content.icon} fa-2xl"></i>
+                <p>${escape(item.content.text)}</p>
+        </div>
+        <div class="right-column">
+          <div class="editIcons">
+            <% if("error"==="error" ){ %>
+              <i class="fa-solid fa-exclamation fa-2xl"></i>
+              <% } %>
+                <i class="fa-solid fa-pen-to-square fa-2xl"></i>
+                <i class="fa-solid fa-trash-can fa-2xl"></i>
+          </div>
+          <div class="timestamp">
+            <p>${timeago.format(item.created_at)}</p>
+          </div>
+        </div>
+      </div>
+    </article>`);
     return $tweet;
   };
