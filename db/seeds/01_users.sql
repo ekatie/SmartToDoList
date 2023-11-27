67bbf031d3b7aacb5f4f8d276c3fbf820c893bad
Katie K <katiekucsera@gmail.com>
2023-11-27 16:30:31 -0500

    Update fake user data

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,4 +1,5 @@
--- Users table seeds here (Example)
-INSERT INTO users (name, email, password) VALUES ('Peter', 'peterhashem@gmail.com', '123');
-INSERT INTO users (name, email, password) VALUES ('Katie', 'katiekucsera@gmail.com', '456');
-INSERT INTO users (name, email, password) VALUES ('Tom', 'thomas.spencer2034@gmail.com', '789');
+INSERT INTO users (id, name, email, password) VALUES 
+(1, 'John Smith', 'jsmith@gmail.com', 'password'),
+(2, 'Jane Doe', 'jdoe@gmail.com', 'password'),
+(3, 'Bruce Wayne', 'bwayne@gmail.com', 'password'),
+(4, 'Tod O.', 'toddo@gmail.com', 'password');
