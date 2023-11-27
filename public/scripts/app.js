3854881854305aa6f7de924ac5800d1fd48bf34f
Tom Spencer <thomas.spencer2034@gmail.com>
2023-11-27 08:54:09 -0500

    Add html for list item

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,15 @@
-// Client facing scripts here
+//Apply code once document is ready
+$(document).ready(function() {
+  //Hide error element
+  $("#error").hide().empty();
+
+  loadList();
+
+ const createListElement = function(tweet) {
+    //Create hard coded tweets
+    const $tweet = $(`<article>
+          <div class = 'item-container'>
+          <div>
+        </article>`);
+    return $tweet;
+  };
