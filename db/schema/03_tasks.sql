93c096b7b13670fb487b6f99e79c4b5fab351f5f
Katie K <katiekucsera@gmail.com>
2023-11-27 19:11:23 -0500

    Refactor formatting, remove comment

--- db/schema/03_tasks.sql
+++ db/schema/03_tasks.sql
@@ -1,6 +1,5 @@
--- Drop and recreate tasks table (Example)
-
 DROP TABLE IF EXISTS tasks CASCADE;
+
 CREATE TABLE tasks (
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
