4178c7d6d563dbdbbf629f3aa4fa4672862ccdf9
Peter Hashem <peterhashem@gmail.com>
2023-11-30 21:30:42 -0500

    added a filterTasks function

--- db/database.js
+++ db/database.js
@@ -12,7 +12,6 @@ const openai = new OpenAI({
  * @return {string} returns the answer of chatGPT API based on the userInput
  */
 async function getCategoryFromAPI(userInput) {
-
   const completion = await openai.chat.completions.create({
     model: "gpt-3.5-turbo-1106",
     messages: [
@@ -22,7 +21,6 @@ async function getCategoryFromAPI(userInput) {
           "You are a smart to-do list that categorizes user input with only one of the following: eat, watch, read, buy, do",
       },
       { role: "user", content: `${userInput}` },
-
     ],
     //temperature controls randomness: Lowering results in less random completions. As temperature approaches 0, the model will become deterministic and repetitive.
     temperature: 1,
@@ -67,15 +65,15 @@ const getUserWithId = function (userId) {
  */
 const getUserTasks = function (userId) {
   const query = `
-  SELECT tasks.* 
-  FROM tasks 
-  JOIN users ON tasks.user_id = users.id 
-  JOIN categories ON tasks.category_id = categories.id 
-  WHERE user_id = $1 
-  ORDER BY 
-    CASE WHEN is_complete THEN 0 ELSE 1 END, 
-    CASE WHEN is_priority THEN 1 ELSE 0 END, 
-    completed_date DESC NULLS LAST, 
+  SELECT tasks.*
+  FROM tasks
+  JOIN users ON tasks.user_id = users.id
+  JOIN categories ON tasks.category_id = categories.id
+  WHERE user_id = $1
+  ORDER BY
+    CASE WHEN is_complete THEN 0 ELSE 1 END,
+    CASE WHEN is_priority THEN 1 ELSE 0 END,
+    completed_date DESC NULLS LAST,
     created_date ASC;`;
 
   return pool.query(query, [userId]).then((result) => {
@@ -96,7 +94,9 @@ const addTask = async function (task) {
   if (!task.category_id) {
     try {
       const category_name = await getCategoryFromAPI(task.description);
-      task.category_id = checkForCategoryKeywords(category_name) ? checkForCategoryKeywords(category_name) : 5;
+      task.category_id = checkForCategoryKeywords(category_name)
+        ? checkForCategoryKeywords(category_name)
+        : 5;
     } catch (err) {
       console.log(err);
     }
@@ -147,14 +147,14 @@ const editTaskCategory = function (taskId, taskCategoryId) {
  * @return {Promise<{}>} A promise to the task status.
  */
 const updateTaskStatus = function (taskId, isComplete) {
-  isComplete = isComplete === 'true';
+  isComplete = isComplete === "true";
 
   const query = `
-    UPDATE tasks 
-    SET 
-      is_complete = $1, 
-      completed_date = ${isComplete ? "NOW()" : "null"} 
-    WHERE tasks.id = $2 
+    UPDATE tasks
+    SET
+      is_complete = $1,
+      completed_date = ${isComplete ? "NOW()" : "null"}
+    WHERE tasks.id = $2
     RETURNING *;`;
 
   const values = [isComplete, taskId];
@@ -181,6 +181,23 @@ const deleteTask = function (taskId) {
   });
 };
 
+const filterTasks = (category) => {
+  const query = `SELECT * FROM tasks
+    JOIN categories ON tasks.category_id = categories.id
+    WHERE categories.id = $1
+    ORDER BY
+    CASE WHEN is_complete THEN 0 ELSE 1 END,
+    CASE WHEN is_priority THEN 1 ELSE 0 END,
+    completed_date DESC NULLS LAST,
+    created_date ASC;`;
+
+  const values = [category];
+
+  return pool.query(query, values).then((filteredTasks) => {
+    return filteredTasks;
+  });
+};
+
 module.exports = {
   getUserWithId,
   getUserTasks,
@@ -188,4 +205,5 @@ module.exports = {
   editTaskCategory,
   updateTaskStatus,
   deleteTask,
+  filterTasks,
 };
